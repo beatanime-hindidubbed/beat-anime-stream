d0a6be04122d44833316745308be658dfba3e244
beatanime-hindidubbed <beatanimephpsite@gmail.com>
2026-03-06 22:04:03 +0530

    Advance Integration app.tx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import CookieConsent from "@/components/CookieConsent";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
+import VerifyGate from "@/components/VerifyGate";   // <-- A. Import added
 import Index from "./pages/Index";
 import SearchPage from "./pages/SearchPage";
 import AnimeDetail from "./pages/AnimeDetail";
@@ -43,46 +44,66 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
-            <ScrollToTop />
-            <Routes>
-              {/* Admin routes - no navbar/footer */}
-              <Route path="/admin" element={<AdminLogin />} />
-              <Route path="/admin/dashboard" element={<AdminDashboard />} />
-              <Route path="/verify" element={<VerifyPage />} />
+            <VerifyGate>   {/* <-- B. Wrapped inside BrowserRouter */}
+              <ScrollToTop />
+              <Routes>
+                {/* Admin routes - no navbar/footer */}
+                <Route path="/admin" element={<AdminLogin />} />
+                <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                <Route path="/verify" element={<VerifyPage />} />
 
-              {/* Main site routes */}
-              <Route
-                path="*"
-                element={
-                  <>
-                    <Navbar />
-                    <main className="min-h-screen">
-                      <Routes>
-                        <Route path="/" element={<Index />} />
-                        <Route path="/search" element={<SearchPage />} />
-                        <Route path="/anime/:id" element={<AnimeDetail />} />
-                        <Route path="/watch/:episodeId" element={<WatchPage />} />
-                        <Route path="/category/:name" element={<CategoryPage />} />
-                        <Route path="/genre/:name" element={<GenrePage />} />
-                        <Route path="/schedule" element={<SchedulePage />} />
-                        <Route path="/hindi" element={<HindiPage />} />
-                        <Route path="/hindi/anime/:id" element={<HindiAnimePage />} />
-                        <Route path="/hindi/watch/:animeId/:episodeNumber" element={<HindiWatchPage />} />
-                        <Route path="/recent" element={<RecentPage />} />
-                        <Route path="/explore" element={<ExplorePage />} />
-                        <Route path="/manhwa" element={<ManhwaPage />} />
-                        <Route path="/policy/:type" element={<PolicyPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/watchlist" element={<WatchlistPage />} />
-                        <Route path="*" element={<Index />} />
-                      </Routes>
-                    </main>
-                    <Footer />
-                    <CookieConsent />
-                  </>
-                }
-              />
-            </Routes>
+                {/* Main site routes */}
+                <Route
+                  path="*"
+                  element={
+                    <>
+                      <Navbar />
+                      <main className="min-h-screen">
+                        <Routes>
+                          <Route path="/" element={<Index />} />
+                          <Route path="/search" element={<SearchPage />} />
+                          <Route path="/anime/:id" element={<AnimeDetail />} />
+                          <Route
+                            path="/watch/:episodeId"
+                            element={<WatchPage />}
+                          />
+                          <Route
+                            path="/category/:name"
+                            element={<CategoryPage />}
+                          />
+                          <Route path="/genre/:name" element={<GenrePage />} />
+                          <Route path="/schedule" element={<SchedulePage />} />
+                          <Route path="/hindi" element={<HindiPage />} />
+                          <Route
+                            path="/hindi/anime/:id"
+                            element={<HindiAnimePage />}
+                          />
+                          <Route
+                            path="/hindi/watch/:animeId/:episodeNumber"
+                            element={<HindiWatchPage />}
+                          />
+                          <Route path="/recent" element={<RecentPage />} />
+                          <Route path="/explore" element={<ExplorePage />} />
+                          <Route path="/manhwa" element={<ManhwaPage />} />
+                          <Route
+                            path="/policy/:type"
+                            element={<PolicyPage />}
+                          />
+                          <Route path="/login" element={<LoginPage />} />
+                          <Route
+                            path="/watchlist"
+                            element={<WatchlistPage />}
+                          />
+                          <Route path="*" element={<Index />} />
+                        </Routes>
+                      </main>
+                      <Footer />
+                      <CookieConsent />
+                    </>
+                  }
+                />
+              </Routes>
+            </VerifyGate>   {/* <-- VerifyGate closing tag */}
           </BrowserRouter>
         </TooltipProvider>
       </SiteSettingsProvider>
